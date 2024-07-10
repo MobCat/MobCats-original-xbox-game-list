48ef9e70b84ae7a7d25d9b6dab701cd841111ac1
MobCat <mobcat99@gmail.com>
2024-07-10 16:09:28 +1000

    Add files via upload
    404 bug "fixed"

--- website/config.php
+++ website/config.php
@@ -1,8 +1,13 @@
 <?php
 // CDN Setup
-//TODO: figuer out if we can stop / 404 when someone trys to viset config.php manually in browser.
-header('HTTP/1.0 404 Not Found', true, 404);
-http_response_code(404);
+//TODO: figure out if we can stop / 404 when someone trys to visit config.php manually in browser.
+//header('HTTP/1.0 404 Not Found', true, 404);
+//http_response_code(404);
+//BUGBUG: The above code brakes meta tags.
+// The meta render thing tries to include our config php, sees it's 404'ing and then nopes out.
+// With this 404 now set, discord or wherever will now no longer render anything, because the website is "404".
+// It doesn't know the difference between say, title.php or config.php being included by title.php.
+// It simply saw a 404, and then stops.
 
 // Change this IP to your dev server.
 // In my case I use http://192.168.1.99/ to get to the dev server.
@@ -20,7 +25,7 @@ if ($_SERVER['HTTP_HOST'] != "192.168.1.99"){
     $DevEnv    = "Dev";
 }
 
-// If your feeling adventurous, you could build out this config to do auto fail over, or any number of checks.
+// If your feeling adventurous, you could build out this config to do auto fail over, or any number of checks or advanced conf setups.
 // Check if the CDN is even there before trying to load, if it's not, pick a new CDN.
 // Check where the user is located and load the CDN that is closest. Some $_SERVER['REMOTE_ADDR'] bs.
 // It's a php script, you can do whatever you want.
