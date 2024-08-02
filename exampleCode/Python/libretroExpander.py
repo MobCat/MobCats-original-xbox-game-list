420c045a9f02e4beb528cd21f28cf08e7fe1c55b
MobCat <mobcat99@gmail.com>
2024-08-02 21:25:09 +1000

    Uploaded downloader
    Updated db downloader to download in chunks, and added a silly little progress bar to the downloader while I was there.
    It wont auto resume or anything like that, but it should be a little more robust then waiting / hanging python on downloading 1.6 MB of stuff into memory.

--- exampleCode/Python/libretroExpander.py
+++ exampleCode/Python/libretroExpander.py
@@ -17,14 +17,24 @@ from PIL import Image  # pip install Pillow==10.4.0
 from io import BytesIO
 import sqlite3
 
+#hide cursor, it makes the download bar look ugly.
+print("\033[?25l", end="")
+
 # Check if we have the db downloaded
 if not os.path.exists('titleIDs.db'):
     # Download it if we don't
     print('Downloading titleIDs.db...')
     url = 'https://mobcat.zip/XboxIDs/titleIDs.db'
-    response = requests.get(url)
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    progress_bar_size = 50
+
     with open('titleIDs.db', 'wb') as f:
-        f.write(response.content)
+        for data in response.iter_content(1024): #1 Kibibyte chunk size
+            f.write(data)
+            progress = int(f.tell() * progress_bar_size / total_size)
+            print(f"\r[{'█' * progress}{' ' * (progress_bar_size - progress)}] {f.tell()/1024/1024:.2f} MB / {f.tell() * 100 / total_size:.2f}% ", end="")
+    print("\nDatabase download complete.\n")
         
 # Check if our download folder excists.
 if not os.path.exists('Named_Boxarts'):
@@ -33,7 +43,7 @@ if not os.path.exists('Named_Boxarts'):
 
 # Define where we are going to download cover thumbnails from.
 # Same variable schema as the api and config.php
-#CNDPrefix = "http://192.168.1.99/XboxIDs/CoverScans/"; 
+#CNDPrefix = "http://192.168.1.99/XboxIDs/CoverScans/"; # 1297
 CNDPrefix = "https://raw.githubusercontent.com/MobCat/MobCats-original-xbox-game-list/main/";
 
 # Load title ID db
@@ -51,7 +61,7 @@ cntMax = len(thumbLst)
 for thumb in thumbLst:
     # If the thumbnail does not excist in our download folder, download it
     if not os.path.exists(f"Named_Boxarts/{thumb['Filename']}.png"):
-        print(f"[{cnt}/{cntMax}]Download: {thumb['Filename']}.png")
+        print(f"[{cnt}/{cntMax}]Download {thumb['XMID']}: {thumb['Filename']}.png")
         # Download the thumbnail and rename it to the redump iso filename
         response = requests.get(f"{CNDPrefix}thumbnail/{thumb['XMID'][:2]}/{thumb['XMID']}.jpg")
         # Convert jpg to png and resize it to 320x448, in accordance with resize.sh
